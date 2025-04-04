620ef15e796f9772f39d2cf26777966915834253
swaroopkasaraneni <swaroopkasaraneni@gmail.com>
2025-04-05 00:17:41 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from .server import serve
+
+
+def main():
+    """MCP Math Server - Maths functionality for MCP"""
+    import argparse
+    import asyncio
+
+    parser = argparse.ArgumentParser(
+        description="give a model the ability to make web requests"
+    )
+    parser.add_argument("--user-agent", type=str, help="Custom User-Agent string")
+    parser.add_argument(
+        "--ignore-robots-txt",
+        action="store_true",
+        help="Ignore robots.txt restrictions",
+    )
+
+    asyncio.run(serve())
+
+
+if __name__ == "__main__":
+    main()
