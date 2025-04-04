271bdbfde6fc278d765dbb924a2cd1443bdffd2e
swaroopkasaraneni <swaroopkasaraneni@gmail.com>
2025-04-05 00:32:02 +0530

    Add files via upload

--- src/mcp_server_math/__init__.py
+++ src/mcp_server_math/__init__.py
@@ -9,12 +9,6 @@ def main():
     parser = argparse.ArgumentParser(
         description="give a model the ability to make web requests"
     )
-    parser.add_argument("--user-agent", type=str, help="Custom User-Agent string")
-    parser.add_argument(
-        "--ignore-robots-txt",
-        action="store_true",
-        help="Ignore robots.txt restrictions",
-    )
 
     asyncio.run(serve())
 
